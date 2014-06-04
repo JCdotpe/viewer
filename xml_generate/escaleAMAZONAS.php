b9b013220dab090075f9cb539145e2cf92f21b70
leandroaltuna <jhonatanaltuna@gmail.com>
2014-06-04 12:18:41 -0500

    generate xml

---
+++ xml_generate/escaleAMAZONAS.php
@@ -0,0 +1,53 @@
+<?php
+require("phpsqlajax_dbinfo.php");
+ini_set('memory_limit', '64M');
+
+function parseToXML($htmlStr) 
+{ 
+$xmlStr=str_replace('<','&lt;',$htmlStr); 
+$xmlStr=str_replace('>','&gt;',$xmlStr); 
+$xmlStr=str_replace('"','&quot;',$xmlStr); 
+$xmlStr=str_replace("'",'&#39;',$xmlStr); 
+$xmlStr=str_replace("&",'&amp;',$xmlStr); 
+return $xmlStr; 
+} 
+
+// $query = "select * from v_Fotos_Capitulos order by id_local";
+$query = "select * from v_ubigeo_local order by codigo_de_local";
+
+$result = mssql_query($query);
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<?xml version="1.0" encoding="utf-8" ?>';
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mssql_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'id_local="' . parseToXML($row['codigo_de_local']) . '" '; 
+  echo 'nro_pred="' . parseToXML($row['Nro_Pred']) . '" ';
+  echo 'dpto_nombre="' . parseToXML($row['c_dpto_nombre']) . '" '; 
+  echo 'prov_nombre="' . parseToXML($row['c_prov_nombre']) . '" '; 
+  echo 'dist_nombre="' . parseToXML($row['c_dist_nombre']) . '" ';
+  echo 'cod_dpto="' . parseToXML($row['c_cod_dpto']) . '" ';
+  echo 'cod_prov="' . parseToXML($row['c_cod_prov']) . '" ';
+  echo 'cod_dist="' . parseToXML($row['c_cod_dist']) . '" ';
+  echo 'ccpp="' . parseToXML($row['centroPoblado']) . '" ';
+
+  // echo 'id_local="' . parseToXML($row['id_local']) . '" ';
+  // echo 'nro_pred="' . parseToXML($row['nro_pred']) . '" '; 
+  // echo 'cap="' . parseToXML($row['cap']) . '" '; 
+  // echo 'ruta_foto="' . parseToXML($row['ruta_foto']) . '" ';
+  // echo 'cod_dpto="' . parseToXML($row['C_cod_dpto']) . '" ';
+  // echo 'cod_prov="' . parseToXML($row['C_cod_prov']) . '" ';
+  // echo 'cod_dist="' . parseToXML($row['C_cod_dist']) . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
---
+++ xml_generate/phpsqlajax_dbinfo.php
@@ -0,0 +1,15 @@
+<?php
+
+$servidor = "192.168.201.43";
+$usuario = "bpalomino";
+$password = "inei2201";
+$bd = "CIE2013"; 
+
+$bdconexion = mssql_connect($servidor, $usuario, $password) or die("Error:" . mssql_get_last_message());
+mssql_select_db($bd, $bdconexion) or die("Error ". mssql_get_last_message());
+// mssql_query ("SET NAMES 'utf8'");
+
+// $bdconexion_PDO = new PDO("mysql:host=$servidor;dbname=$bd", $usuario, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+
+?>
